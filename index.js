3e8a1a08caddfab43fc821493fe21f1bf2f4d90e
Hyungsuk Yu <>
2024-07-11 15:50:22 +0900

    main test

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 class StepIndicator {
-  el = null;
-  steps = 5;
+  el = 11;
+  steps = 10;
 
   constructor(el) {
     this.el = document.querySelector(el);
